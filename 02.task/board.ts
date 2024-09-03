fcfb33e0300205c776c788f0e1d65b254a11ea35
Begmatov Almaz <lolkek13@inbox.ru>
2024-09-03 15:52:57 +0600

    Создал шахматную доску и создал npm run board для выведения доски

--- 02.task/board.ts
+++ 02.task/board.ts
@@ -1,2 +1,18 @@
-const root = document.getElementById('root');
-const lala = 1
+const createChessBoard = (): string => {
+    let board = ''
+    const black = '██';
+    const white = '  ';
+    let size = 8;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if ((i + j) % 2 == 0) {
+                board += white;
+            } else {
+                board += black;
+            }
+        } board += '\n'
+    }
+    return board
+}
+console.log(createChessBoard());
+
